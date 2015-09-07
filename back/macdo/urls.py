c89501de800ce43e3f2c008c979197f7c7d88c55
Thibaut Fatus <tf@little-big-data.com>
2015-09-07 20:06:46 +0200

    start react project

--- back/macdo/urls.py
+++ back/macdo/urls.py
@@ -1,10 +1,10 @@
+from django.conf import settings
+from django.views.generic.base import RedirectView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'macdo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/?$', RedirectView.as_view(url='/index.html')),
+    url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
