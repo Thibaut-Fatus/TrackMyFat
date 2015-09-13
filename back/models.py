26343bc633ab3f3531915b4ce1f630e48993ccb1
Thibaut Fatus <tfatus@gmail.com>
2015-09-13 15:09:05 +0200

    ADD meal model

---
+++ back/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Meal(models.Model):
+    """
+    stores all info relative to a meal:
+    name
+    url
+    image
+    nutritional info
+    """
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    url = models.CharField(max_length=500)
